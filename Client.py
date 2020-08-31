e249f0286e2d55cc6e82cdfff9c0bae74307ce7a
Mark Gao <markgao5059@outlook.com>
2020-08-30 22:26:41 -0400

    Very first attempt at a model.

---
+++ Client.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from datetime import datetime as dt
+
+samples = pd.DataFrame({"File": ["File 1", "File 2", "File 3", "File BADD"],
+                        "Date": [dt.fromisoformat("2020-01-01"),
+                                 dt.fromisoformat("2020-02-14"),
+                                 dt.fromisoformat("2020-03-07"),
+                                 dt.fromisoformat("2020-09-21")]})
+
+date_labels = pd.DataFrame({"Date": [dt.fromisoformat("2020-01-01"),
+                                     dt.fromisoformat("2020-02-14"),
+                                     dt.fromisoformat("2020-03-07"),
+                                     dt.fromisoformat("2021-01-01")],
+                            "Change": ["Increase", "Increase", "Decrease", "Increase"]})
+
+print(samples.info())
+print(date_labels.info())
+
+df = samples.join(date_labels.set_index('Date'), on='Date', how='inner', lsuffix="LDate", rsuffix="RDate")
+
+print(df.head())
--- Tryouts.py
+++ Tryouts.py
@@ -2,7 +2,10 @@ import WebScrape
 import EnergyAPI
 import CleanHTML
 import pandas as pd
-from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 """
 Mark Gao - Capstone Project 2020.
@@ -27,7 +30,7 @@ samples = CleanHTML.associate_dates(root, htmlfiles, classes) # contains dict{fi
 EIA_df = EnergyAPI.main(display=False)
 EIA_df = EIA_df.iloc[:550, :]  # don't need pre-2011 data
 
-date_labels = EnergyAPI.status_by_week(samples, EIA_df) # constains dict{datetime obj: change str}
+date_labels = EnergyAPI.status_by_week(samples, EIA_df) # contains dict{datetime obj: change str}
 
 files = list(samples.keys())
 file_dates = list(samples.values())
@@ -39,3 +42,53 @@ df2 = pd.DataFrame({"Date": change_dates, "Change": changes}, index=range(len(ch
 data = df2.join(df1.set_index('Date'), on='Date', how='inner', lsuffix="LDate", rsuffix="RDate")  # must join on idx
 
 print(data.head())
+
+X = data['Filename']
+y = data['Change']
+
+y_train = y.iloc[:700]
+y_test = y.iloc[700:]
+
+X_train = list()
+X_test = list()
+
+for filename in X.iloc[:700]:
+    filepath = root + "/raw-data/unlabelled/" + filename
+
+    with open(filepath, 'r', encoding='utf8') as file:
+        html = file.read()
+        soup = BeautifulSoup(html, features='lxml')
+        tag_instances = soup.find_all('p')
+
+        textblob = ""
+        for tag in tag_instances:
+            textblob += tag.text
+
+        X_train.append(textblob)
+
+for filename in X.iloc[700:]:
+    filepath = root + "/raw-data/unlabelled/" + filename
+
+    with open(filepath, 'r', encoding='utf8') as file:
+        html = file.read()
+        soup = BeautifulSoup(html, features='lxml')
+        tag_instances = soup.find_all('p')
+
+        textblob = ""
+        for tag in tag_instances:
+            textblob += tag.text
+
+        X_test.append(textblob)
+
+vectorizer = CountVectorizer(min_df=30,
+                             tokenizer=None,
+                             ngram_range=(1, 3))
+
+print(len(X_train))
+X_train = vectorizer.fit_transform(X_train)
+
+print(X_train.shape)
+print(y_train.shape)
+logit = LogisticRegression()
+logit.fit(X_train, y_train)
+print(logit.score(X_train, y_train))
