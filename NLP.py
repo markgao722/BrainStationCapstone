2c68a58cf66aacc1895bc31de31cc61a67db8108
Mark Gao <markgao5059@outlook.com>
2020-09-14 16:34:15 -0400

    Ran a couple iterations of each pipeline

--- NLP.py
+++ NLP.py
@@ -1,13 +1,14 @@
 import pandas as pd
-import numpy as np
-import tensorflow as tf
+# import numpy as np
+# import tensorflow as tf
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
-from sklearn.decomposition import PCA, KernelPCA
+from sklearn.decomposition import PCA  # KernelPCA
+
 
 """
 Mark Gao - Capstone Project 2020.
@@ -17,71 +18,119 @@ Use this module to fit an NLP model.
 
 
 def prepare(X_train, X_test)-> tuple:
+    """
 
-    vectorizer = CountVectorizer(min_df=30,
-                                 tokenizer=None,
+    :param X_train:
+    :param X_test:
+    :return:
+    """
+    vectorizer = CountVectorizer(tokenizer=None,
                                  ngram_range=(1, 3),
                                  )
 
-    X_train_ = vectorizer.fit_transform(X_train)
-    X_test_ = vectorizer.transform(X_test)
+    X_train_mtx = vectorizer.fit_transform(X_train['Data'])
+    X_test_mtx = vectorizer.transform(X_test['Data'])
 
-    return (X_train_, X_test_)
+    X_train_ = pd.DataFrame(data=X_train_mtx.toarray(),
+                            columns=vectorizer.get_feature_names())
+    X_test_ = pd.DataFrame(data=X_test_mtx.toarray(),
+                           columns=vectorizer.get_feature_names())
+
+    return X_train_, X_test_
 
 
 def model_one(X_train, X_test, y_train, y_test, metrics=True)-> tuple:
     """
-    Run the first trial, which was done with a decision tree model. Estimated run-time is XXX minutes.
-    :param X_train: df, or Series, where the column contains strings representing articles.
-    :param X_test:  df, or Series, where the column contains strings representing articles.
-    :param y_train: df, or Series, where the column contains the 'increase' / 'decrease' string.
-    :param y_test:  df, or Series, where the column contains the 'increase' / 'decrease' string.
+    Run the first experiment, which was done with a decision tree model. Estimated run-time is XXX minutes.
+    :param X_train:
+    :param X_test:
+    :param y_train:
+    :param y_test:
     :param metrics: bool, print out the key metrics when true.
     :return:        tuple, containing various results of the model
     """
 
-    pipe = Pipeline([('PCA', PCA()),
+    pipe = Pipeline([
+                     ('PCA', PCA()),
                      ('Model', DecisionTreeClassifier()),
                      ])
 
-    grid = GridSearchCV(pipe, param_grid={'PCA__n_components': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                                          'Model__max_depth': range(25),
+    grid = GridSearchCV(pipe, param_grid={'PCA__n_components': list(range(1, 10)),
+                                          'Model__max_depth': list(range(1, 10)),
+                                          'Model__criterion': ['gini', 'entropy'],
+                                          'Model__splitter': ['best', 'random'],
                                           })
 
     model = grid.fit(X_train, y_train)
 
+    train_score = model.score(X_train, y_train)
     test_score = model.score(X_test, y_test)
     best_est = model.best_estimator_
 
     if metrics:
-        print(test_score)
-        print(best_est)
+        print(f"Model 1 train score: {train_score}")
+        print(f"Model 1 test score: {test_score}")
+        print(f"Model 1 best estimator: {best_est}")
 
-    return (test_score, best_est)
+    return test_score, best_est
 
 
 def model_two(X_train, X_test, y_train, y_test, metrics=True)-> tuple:
     """
-    Run the first trial, which was done with a random forest model. Estimated run-time is XXX minutes.
+    Run the second experiment, which was done with a random forest model. Estimated run-time is XXX minutes.
     >> See model_one for the parameter descriptions.
-    :return:
     """
 
-    pipe = Pipeline([('PCA', PCA()),
+    pipe = Pipeline([
+                     ('PCA', PCA()),
                      ('Model', RandomForestClassifier()),
                      ])
 
-    grid = GridSearchCV(pipe, param_grid={'PCA__n_components': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                                          'Model__n_estimators': [1, 2, 3, 4, 5],
-                                          'Model__max_depth': range(25)})
+    grid = GridSearchCV(pipe, param_grid={'PCA__n_components': list(range(1, 10)),
+                                          'Model__n_estimators': [2, 5, 10, 25, 50],
+                                          'Model__max_depth': list(range(1, 10)),
+                                          'Model__criterion': ['gini', 'entropy'],
+                                          })
 
     model = grid.fit(X_train, y_train)
 
+    train_score = model.score(X_train, y_train)
     test_score = model.score(X_test, y_test)
     best_est = model.best_estimator_
 
     if metrics:
-        print(test_score)
-        print(best_est)
+        print(f"Model 2 train score: {train_score}")
+        print(f"Model 2 test score: {test_score}")
+        print(f"Model 2 best estimator: {best_est}")
 
-    return (test_score, best_est)
+    return test_score, best_est
+
+
+def model_three(X_train, X_test, y_train, y_test, metrics=True)-> tuple:
+    """
+    Run the third experiment
+    >> See model_one for the parameter descriptions.
+    """
+
+    pipe = Pipeline([
+        ('PCA', PCA()),
+        ('Model', LogisticRegression(max_iter=300, solver='saga')),
+    ])
+
+    grid = GridSearchCV(pipe, param_grid={'PCA__n_components': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                                          'Model__penalty': ['l1', 'l2', 'elasticnet'],
+                                          'Model__C': [1, 10, 100, 1000]
+                                          })
+
+    model = grid.fit(X_train, y_train)
+
+    train_score = model.score(X_train, y_train)
+    test_score = model.score(X_test, y_test)
+    best_est = model.best_estimator_
+
+    if metrics:
+        print(f"Model 3 train score: {train_score}")
+        print(f"Model 3 test score: {test_score}")
+        print(f"Model 3 best estimator: {best_est}")
+
+    return test_score, best_est
--- Tryouts.py
+++ Tryouts.py
@@ -1,11 +1,11 @@
-import WebScrape
+# import WebScrape
 import EnergyAPI
 import CleanHTML
+import NLP
 import pandas as pd
 from bs4 import BeautifulSoup
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
 
 """
 Mark Gao - Capstone Project 2020.
@@ -26,11 +26,12 @@ classes = ["unlabelled"]
 # =============================================================================
 
 
-samples = CleanHTML.associate_dates(root, htmlfiles, classes) # contains dict{filename str: datetime obj}
+# --- Step 2: Tag each article with a date and its corresponding oil event on that date. ---
+samples = CleanHTML.associate_dates(root, htmlfiles, classes)  # contains dict{filename str: datetime obj}
 EIA_df = EnergyAPI.main(display=False)
 EIA_df = EIA_df.iloc[:550, :]  # don't need pre-2011 data
 
-date_labels = EnergyAPI.status_by_week(samples, EIA_df) # contains dict{datetime obj: change str}
+date_labels = EnergyAPI.status_by_week(samples, EIA_df)  # contains dict{datetime obj: change str}
 
 files = list(samples.keys())
 file_dates = list(samples.values())
@@ -39,20 +40,19 @@ changes = list(date_labels.values())
 
 df1 = pd.DataFrame({"Filename": files, "Date": file_dates}, index=range(len(files)))
 df2 = pd.DataFrame({"Date": change_dates, "Change": changes}, index=range(len(changes)))
+
+# -- Result:
 data = df2.join(df1.set_index('Date'), on='Date', how='inner', lsuffix="LDate", rsuffix="RDate")  # must join on idx
 
-print(data.head())
 
+# --- Step 3: Using the DataFrame of article files, open each file and extract the text into a DataFrame.
+#             Each entry of the DataFrame is a giant block of text representing one article.
 X = data['Filename']
 y = data['Change']
 
-y_train = y.iloc[:700]
-y_test = y.iloc[700:]
+text_list = list()
 
-X_train = list()
-X_test = list()
-
-for filename in X.iloc[:700]:
+for filename in X.iloc[:]:
     filepath = root + "/raw-data/unlabelled/" + filename
 
     with open(filepath, 'r', encoding='utf8') as file:
@@ -64,31 +64,18 @@ for filename in X.iloc[:700]:
         for tag in tag_instances:
             textblob += tag.text
 
-        X_train.append(textblob)
+        text_list.append(textblob)
 
-for filename in X.iloc[700:]:
-    filepath = root + "/raw-data/unlabelled/" + filename
+# --- Result:
+X_df = pd.DataFrame({"Data": text_list}, index=range(len(text_list)))
+# Note, the beginning of every article starts with "Click here for 150+..." but the articles re actually different.
 
-    with open(filepath, 'r', encoding='utf8') as file:
-        html = file.read()
-        soup = BeautifulSoup(html, features='lxml')
-        tag_instances = soup.find_all('p')
 
-        textblob = ""
-        for tag in tag_instances:
-            textblob += tag.text
+# --- Step 4: Split the data into train and test sets and begin running models.
+X_train_df, X_test_df, y_train, y_test = train_test_split(X_df, y, test_size=0.33, random_state=0)
 
-        X_test.append(textblob)
+X_train, X_test = NLP.prepare(X_train_df, X_test_df)
 
-vectorizer = CountVectorizer(min_df=30,
-                             tokenizer=None,
-                             ngram_range=(1, 3))
-
-print(len(X_train))
-X_train = vectorizer.fit_transform(X_train)
-
-print(X_train.shape)
-print(y_train.shape)
-logit = LogisticRegression()
-logit.fit(X_train, y_train)
-print(logit.score(X_train, y_train))
+NLP.model_one(X_train, X_test, y_train, y_test)
+NLP.model_two(X_train, X_test, y_train, y_test)
+NLP.model_three(X_train, X_test, y_train, y_test)
